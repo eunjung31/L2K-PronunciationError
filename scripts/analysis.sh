7af56ba5e85cd9ed320318c7b103d89c43a54c81
Yeo, Eun Jung <ej.yeo@snu.ac.kr>
2023-04-25 00:19:06 +0900

    organize folders

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## Decode using fine-tuneind phone recognizer
-python3 transcribe.py
+python3 local/transcribe.py
 
 ## Force-Align reference (g2P) & phonetic transcriptions
-python3 alignProcess.py
-bash align.sh
+python3 local/alignProcess.py
+bash local/align.sh
 
 ## generate confusionmatrix & analysis
-python3 cmProcess.py
-python3 confusionMatrix.py
+python3 local/cmProcess.py
+python3 local/confusionMatrix.py
